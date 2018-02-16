zrepl/zrepl

commit 51377a8ffb969cc2c7f4738bee27bb0191a3df0c

logger: support replacing fields
do no delete() from array since this could lead to resizing

refs #10

logger/logger.go
@@ -103,12 +103,17 @@ func (l *Logger) WithOutlet(outlet Outlet, level Level) *Logger {
 	return child
 }
 
+func (l *Logger) ReplaceField(field string, val interface{}) *Logger {
+	l.fields[field] = nil
+	return l.WithField(field, val)
+}
+
 func (l *Logger) WithField(field string, val interface{}) *Logger {
 
 	l.mtx.Lock()
 	defer l.mtx.Unlock()
 
-	if _, ok := l.fields[field]; ok {
+	if val, ok := l.fields[field]; ok && val != nil {
 		l.logInternalError(nil,
 			fmt.Sprintf("caller overwrites field '%s'. Stack: %s", field, string(debug.Stack())))
 	}

commit fc1c46ffd759834999fec3dbf49e3bcd520dbc86

logger: fix ReplaceWith: would case parent field to be nil
Now WithField and ReplaceWith are wrappers around a common
forkLogger routine

regression introduced in 51377a8

logger/logger.go
@@ -92,20 +92,8 @@ func (l *Logger) WithOutlet(outlet Outlet, level Level) *Logger {
 	return child
 }
 
-func (l *Logger) ReplaceField(field string, val interface{}) *Logger {
-	l.fields[field] = nil
-	return l.WithField(field, val)
-}
-
-func (l *Logger) WithField(field string, val interface{}) *Logger {
-
-	l.mtx.Lock()
-	defer l.mtx.Unlock()
-
-	if val, ok := l.fields[field]; ok && val != nil {
-		l.logInternalError(nil,
-			fmt.Sprintf("caller overwrites field '%s'. Stack: %s", field, string(debug.Stack())))
-	}
+// callers must hold l.mtx
+func (l *Logger) forkLogger(field string, val interface{}) *Logger {
 
 	child := &Logger{
 		fields:        make(Fields, len(l.fields)+1),
@@ -119,7 +107,22 @@ func (l *Logger) WithField(field string, val interface{}) *Logger {
 	child.fields[field] = val
 
 	return child
+}
 
+func (l *Logger) ReplaceField(field string, val interface{}) *Logger {
+	l.mtx.Lock()
+	defer l.mtx.Unlock()
+	return l.forkLogger(field, val)
+}
+
+func (l *Logger) WithField(field string, val interface{}) *Logger {
+	l.mtx.Lock()
+	defer l.mtx.Unlock()
+	if val, ok := l.fields[field]; ok && val != nil {
+		l.logInternalError(nil,
+			fmt.Sprintf("caller overwrites field '%s'. Stack: %s", field, string(debug.Stack())))
+	}
+	return l.forkLogger(field, val)
 }
 
 func (l *Logger) WithFields(fields Fields) (ret *Logger) {