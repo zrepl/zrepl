e4958248345a283d29418708c1111a24a94dedd3
Anton Schirg <anton.schirg@posteo.de>
2018-08-29 19:17:45 +0200

    move wakeup to client package and extract http client

--- client/jsonclient.go
+++ client/jsonclient.go
@@ -1,37 +1,14 @@
-package main
+package client
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"github.com/pkg/errors"
-	"github.com/zrepl/zrepl/config"
-	"github.com/zrepl/zrepl/daemon"
-	"io"
-	"net"
 	"net/http"
+	"net"
+	"context"
+	"bytes"
+	"encoding/json"
+	"io"
+	"github.com/pkg/errors"
 )
-
-func RunWakeup(config config.Config, args []string) error {
-	if len(args) != 1 {
-		return errors.Errorf("Expected 1 argument: job")
-	}
-
-	httpc, err := controlHttpClient(config.Global.Control.SockPath)
-	if err != nil {
-		return err
-	}
-
-	err = jsonRequestResponse(httpc, daemon.ControlJobEndpointWakeup,
-		struct {
-			Name string
-		}{
-			Name: args[0],
-		},
-		struct{}{},
-	)
-	return err
-}
 
 func controlHttpClient(sockpath string) (client http.Client, err error) {
 	return http.Client{
@@ -66,3 +43,4 @@ func jsonRequestResponse(c http.Client, endpoint string, req interface{}, res in
 
 	return nil
 }
+
---
+++ client/wakeup.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"github.com/pkg/errors"
+	"github.com/zrepl/zrepl/config"
+	"github.com/zrepl/zrepl/daemon"
+)
+
+func RunWakeup(config config.Config, args []string) error {
+	if len(args) != 1 {
+		return errors.Errorf("Expected 1 argument: job")
+	}
+
+	httpc, err := controlHttpClient(config.Global.Control.SockPath)
+	if err != nil {
+		return err
+	}
+
+	err = jsonRequestResponse(httpc, daemon.ControlJobEndpointWakeup,
+		struct {
+			Name string
+		}{
+			Name: args[0],
+		},
+		struct{}{},
+	)
+	return err
+}
