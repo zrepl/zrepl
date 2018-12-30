25c974f0b57eb09d1e04c1b3dc78356e6bb96a17
Christian Schwarz <me@cschwarz.com>
2018-12-30 20:43:51 +0100

    envconst: support for int64

--- util/envconst/envconst.go
+++ util/envconst/envconst.go
@@ -2,6 +2,7 @@ package envconst
 
 import (
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -17,6 +18,22 @@ func Duration(varname string, def time.Duration) time.Duration {
 		return def
 	}
 	d, err := time.ParseDuration(e)
+	if err != nil {
+		panic(err)
+	}
+	cache.Store(varname, d)
+	return d
+}
+
+func Int64(varname string, def int64) int64 {
+	if v, ok := cache.Load(varname); ok {
+		return v.(int64)
+	}
+	e := os.Getenv(varname)
+	if e == "" {
+		return def
+	}
+	d, err := strconv.ParseInt(e, 10, 64)
 	if err != nil {
 		panic(err)
 	}
