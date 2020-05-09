c5b530669e965c72ee35e512b6dbaf1d63d07ce9
Christian Schwarz <me@cschwarz.com>
2020-05-09 11:55:40 +0200

    endpoint.ListAbstractionsError: fix stack overflow in .Error()
    fixes #320
    refs #318

--- endpoint/endpoint_zfs_abstraction.go
+++ endpoint/endpoint_zfs_abstraction.go
@@ -468,7 +468,7 @@ func (e ListAbstractionsErrors) Error() string {
 	}
 	msgs := make([]string, len(e))
 	for i := range e {
-		msgs[i] = e.Error()
+		msgs[i] = e[i].Error()
 	}
 	return fmt.Sprintf("list endpoint abstractions: multiple errors:\n%s", strings.Join(msgs, "\n"))
 }
