1341b80296fe87ba4f5753fe3177da24aa4c0194
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-10-20 20:32:38 +0200

    Remove check for receiving-side root filesystems must not overlap
    Because some jobs add client identity to root_fs and other jobs don't do that,
    we can't reliable detect overlapping of filesystems. And and the same time we
    need an ability to use equal or overlapped root_fs for different jobs. For
    instance see this config:
    
    ```
      - name: "zdisk"
        type: "sink"
        root_fs: "zdisk/zrepl"
        serve:
          type: "local"
          listener_name: "zdisk"
    ```
    and
    ```
      - name: "remote-to-zdisk"
        type: "pull"
        connect:
          type: "tls"
        root_fs: "zdisk/zrepl/remote"
    ```
    
    As you can see, two jobs have overlapped root_fs, but actually datasets are not
    overlapped, because job `zdisk` save everything under `zdisk/zrepl/localhost`,
    because it adds client identity. So they actually use two different filesystems:
    `zdisk/zrepl/localhost` and `zdisk/zrepl/remote`. And we can't detect this
    situation during config check. So let's just remove this check, because it's
    admin's duty to configure correct root_fs's.

--- daemon/job/build_jobs.go
+++ daemon/job/build_jobs.go
@@ -24,21 +24,6 @@ func JobsFromConfig(c *config.Config, parseFlags config.ParseFlags) ([]Job, erro
 		js[i] = j
 	}
 
-	// receiving-side root filesystems must not overlap
-	{
-		rfss := make([]string, 0, len(js))
-		for _, j := range js {
-			jrfs, ok := j.OwnedDatasetSubtreeRoot()
-			if !ok {
-				continue
-			}
-			rfss = append(rfss, jrfs.ToString())
-		}
-		if err := validateReceivingSidesDoNotOverlap(rfss); err != nil {
-			return nil, err
-		}
-	}
-
 	return js, nil
 }
 
