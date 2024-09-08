d11267908ef08251eb633c99f3e3d9e7ee8bd6ba
Christian Schwarz <me@cschwarz.com>
2024-09-08 12:00:28 +0000

    try address https://github.com/zrepl/zrepl/pull/801#discussion_r1749203758

--- daemon/snapper/snapname/snapname.go
+++ daemon/snapper/snapname/snapname.go
@@ -23,10 +23,20 @@ func New(prefix, tsFormat, tsLocation string) (*Formatter, error) {
 		prefix:    prefix,
 		timestamp: timestamp,
 	}
-	// Best-effort check to detect whether the result would be an invalid name early.
-	testFormat := "some/dataset@" + formatter.Format(time.Now())
-	if err := zfs.EntityNamecheck(testFormat, zfs.EntityTypeSnapshot); err != nil {
-		return nil, errors.Wrapf(err, "prefix or timestamp format result in invalid snapshot name such as %q", testFormat)
+	// Best-effort check to detect whether the result would be an invalid name.
+	// Test two dates that in most places have will have different time zone offsets due to DST.
+	check := func(t time.Time) error {
+		testFormat := "some/dataset@" + formatter.Format(t)
+		if err := zfs.EntityNamecheck(testFormat, zfs.EntityTypeSnapshot); err != nil {
+			return errors.Wrapf(err, "prefix or timestamp format result in invalid snapshot name such as %q", testFormat)
+		}
+		return nil
+	}
+	if err := check(time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC)); err != nil {
+		return nil, err
+	}
+	if err := check(time.Date(2020, 12, 1, 0, 0, 0, 0, time.UTC)); err != nil {
+		return nil, err
 	}
 	return formatter, nil
 }
