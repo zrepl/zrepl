d9a912ab74630e5799fcd876b48d8d5074263767
Christian Schwarz <me@cschwarz.com>
2026-01-22 00:36:43 +0000

    exited with success|error

--- internal/zfs/zfscmd/zfscmd_logging.go
+++ internal/zfs/zfscmd/zfscmd_logging.go
@@ -35,7 +35,7 @@ func waitPostLogging(c *Cmd, u usage, err error, now time.Time) {
 		WithField("usertime_s", u.user_secs)
 
 	if err == nil {
-		log.Info("command completed successfully")
+		log.Info("command exited with success")
 	} else {
 		log.WithError(err).Info("command exited with error")
 	}
